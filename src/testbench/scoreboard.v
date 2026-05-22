454d274b31cb496f40ebdc06cdfac078d36f2525
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-05-22 09:55:46 +0530

    Update scoreboard.v

--- src/testbench/scoreboard.v
+++ src/testbench/scoreboard.v
@@ -1,34 +1,31 @@
-module scoreboard#(parameter D=8);
+module scoreboard #(parameter D=8);
 
+	integer pass_count;
+	integer fail_count;
 
+	initial begin
+		pass_count=0;
+		fail_count=0;
+	end
 	task check_data;
 
-		input[D-1:0]dut_data;
-		input[D-1:0]ref_data;
-
+		input [D-1:0] dut_data;
+		input [D-1:0] ref_data;
 		begin
-
-			@(posedge uart_top_tb.REF_MODEL.baud_clk);
-
-				if(dut_data==ref_data) begin
-
-					$display("PASS");
-					$display("REF DATA=%h",ref_data);
-					$display("DUT DATA=%h",dut_data);
-					$display("TIME=%0t",$time);
-
-				end
-
-				else begin
-
-					$display("FAIL");
-					$display("REF DATA=%h",ref_data);
-					$display("DUT DATA=%h",dut_data);
-					$display("TIME=%0t",$time);
-
-				end
-
+			if(dut_data==ref_data) begin
+				pass_count=pass_count+1;
+				$display("PASS");
+				$display("REF DATA=%h", ref_data);
+				$display("DUT DATA=%h", dut_data);
+				$display("TIME=%0t", $time);
+			end
+			else begin
+				fail_count=fail_count+1;
+				$display("FAIL");
+				$display("REF DATA=%h", ref_data);
+				$display("DUT DATA=%h", dut_data);
+				$display("TIME=%0t", $time);
+			end
 		end
 	endtask
-
 endmodule
